c5c4af3707e3402d488588fcfbaa0a96553daa5d
Your Name <you@example.com>
2022-09-07 07:52:29 +0500

    added query parameters

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from enum import Enum
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
 
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
@@ -33,8 +40,8 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, 'q': q}
 
 
 @app.get("/users/me")
