57bfe3a6df6cdb63306c2adc124d9da695163b9a
alex <inkeeper@inbox.ru>
2022-09-13 23:50:46 +0500

    post first

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    print(item.name)
+
+    return item
+
+
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
